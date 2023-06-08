7b9ca2bc1bcc126cad98af5a949becd0c606316f
ghada <nasrighada10@outlook.com>
2023-06-08 19:45:53 +0100

    second commit

--- src/MapContainer.jsx
+++ src/MapContainer.jsx
@@ -2,6 +2,10 @@ import React from "react";
 import { GoogleMap, Marker, LoadScript } from "@react-google-maps/api";
 
 const MapContainer = () => {
+  // Click on the navigation menu (☰) in the upper-left corner of the page and go to "APIs & Services" > "Library".
+  // In the Library page, search for "Maps JavaScript API".
+  // Click on the "Maps JavaScript API" result that appears.
+  // On the API details page, click the "Enable" button.
   const mapContainerStyle = {
     width: "100%",
     height: "400px",
